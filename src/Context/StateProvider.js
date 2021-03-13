98fe2e1ab74dfa30c6296a5d2526ef2edd6430d1
ercumentlacin <forever.ducale@gmail.com>
2021-03-13 16:46:24 +0300

    state

--- src/Context/StateProvider.js
+++ src/Context/StateProvider.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 
 export const StateContext = createContext();
 
-export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </StateContext.Provider>
-);
+export const StateProvider = ({ reducer, initialState, children }) => {
+  return (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </StateContext.Provider>
+  );
+};
 
 export const useStateValue = () => useContext(StateContext);
 
